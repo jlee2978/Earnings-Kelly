102fe7737c6f468039d9ee3c42e05d480aeebe83
jlee2978 <35323949+jlee2978@users.noreply.github.com>
2022-04-17 02:38:30 -0400

    cleaned up code

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ import math
 from scipy import stats
 from scipy.stats import norm
 # from numpy import array
+import random
+
+# list of # moves post-earnings of stock
+moves = [10.47,1.61,2.09,2.26,2.04,4.91,6.83,6.63,5.89,4.42,4.34,2.61,4.73,0.31,6.1,2.25,6.5,6.26,6.57,4.12,4.23,1.58,5.65,2.72,2.61,8.2,7.99,2.49,5.14,0.16,12.35,0.91,4.32,8.87,6.24,5.59,2.67,2.42,0.53,2.68,0.93,5.98,1.41,4.69];
+
+# sizing of trades
+sizing = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
 
 def main():
     capital = float(input("Starting capital: "));
@@ -9,6 +16,7 @@ def main():
     r = float(input("Risk free rate (%): "));
     r = r/100;
     vol = float(input("Volatility post earnings (%): "));
+    vol = vol/100;
 
     # ask how many contracts user is opening
     cLong = int(input("Number of long call contracts opening: "));
@@ -16,7 +24,8 @@ def main():
     cShort = int(input("Number of short call contracts opening: "));
     pShort = int(input("Number of short put contracts opening: "));
     premium = float(input("Premium received (-paid): "));   # can be negative
-    capital += premium;
+    maxLoss = float(input("Max loss based on trade structure: "));
+    portfolio = [];
 
     # lists to holds strikes and days to expiry of call and put contracts
     clk, clt = ([] * cLong for i in range(2));
@@ -47,62 +56,56 @@ def main():
     # for loop iterating n (eg. 1000) times for Monte Carlo simulation
     # randomize stock price based on historical movements and calculate payout of each contract and net it to capital
     iterations = int(input("Iterations to run: "));
+    profit = 0;
+
+    # trade sizing
+    # for k in range(len(sizing)):
+    #     size = capital * sizing[k];
 
     for i in range(iterations):
+        # apply premium for each iteration
+        capital += premium;
         profit = 0;
+
         # randomize stock price post earnings movement
-        # setting s to 100 for now, to randomize later (eg. s * randomized historical move %)
-        s = 100;
+        post = s * (1 + random.choice(moves)/100);
 
         # close open long call contracts
         for j in range(cLong):
-            profit += call(s, clk[j], r, clt[j]/365, vol/100);
+            profit += call(post, clk[j], r, clt[j]/365, vol);
 
         # close open short call contracts
         for j in range(cShort):
-            profit -= call(s, csk[j], r, cst[j]/365, vol/100);
+            profit -= call(post, csk[j], r, cst[j]/365, vol);
 
         # close open long put contracts
         for j in range(pLong):
-            profit += put(s, plk[j], r, plt[j]/365, vol/100);
+            profit += put(post, plk[j], r, plt[j]/365, vol);
 
         # close open short put contracts
         for j in range(pShort):
-            profit -= put(s, psk[j], r, pst[j]/365, vol/100);
+            profit -= put(post, psk[j], r, pst[j]/365, vol);
 
         capital += profit;
-
-    # append all profits to capital to make a list of movements to graph
+        portfolio.append(capital);
 
     print(capital);
 
+    # make a list of movements to graph and export to csv
 
-
-
-    # TESTING BSM MODEL BELOW
-
-    # s = stock price, k = strike price, r = interest rate in %, t = years to expiry, vol annual volatility in %    
-    # s = float(input("Stock spot price: "));
-    # k = float(input("Strike price: "));
-    # r = float(input("Risk free rate (%): "));
-    # t = float(input("Days to expiry: "));
-    # vol = float(input("Volatility (%): "));
-
-    # call(s, k, r/100, t/365, vol/100);
-    # put(s, k, r/100, t/365, vol/100);
 
 def call (s, k, r, t, vol):    
     d1 = (math.log(s/k) + (r + (vol**2)/2) * t) / (vol * math.sqrt(t));
     d2 = d1 - vol * math.sqrt(t);
     c = norm.cdf(d1) * s - norm.cdf(d2) * k * math.exp(-r*t);
-    print(c);
+    # print(c);
     return c * 100;
 
 def put (s, k, r, t, vol):
     d1 = (math.log(s/k) + (r + (vol**2)/2) * t) / (vol * math.sqrt(t));
     d2 = d1 - vol * math.sqrt(t);
     p = norm.cdf(-d2) * k * math.exp(-r*t) - norm.cdf(-d1) * s;
-    print(p);
+    # print(p);
     return p * 100;
 
 main();
